33a506d6bb5b2f43bb44cf067591032575a674c4
oatmeala <99986573+oatmeala@users.noreply.github.com>
2023-05-21 00:06:20 +0200

    remove powershell script crate

--- src/commands.rs
+++ src/commands.rs
@@ -7,6 +7,9 @@ use serenity::{
 pub mod help;
 pub use help::*;
 
+pub mod curse;
+pub use curse::*;
+
 pub async fn command(ctx: Context,msg: Message) {
     match msg.content.as_str() {
         "!help" => help::help_msg(ctx,msg).await,
